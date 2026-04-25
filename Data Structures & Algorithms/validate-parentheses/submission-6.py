41937936633fc97e364ec61bd5ca098ac3fdeb4c
Praveen Kumar <52415710+kumarpraveen7@users.noreply.github.com>
2026-04-25 18:54:25 +0530

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        pairs = {')': '(', ']': '[', '}': '{'}
+        if(len(s)%2!=0):
+            return False
+        st = deque()
+        for i in range(len(s)):
+            if(s[i] == '(' or s[i] == '[' or s[i] == '{'):
+                st.append(s[i])
+            if(s[i] == ')'):
+                if(len(st)!=0 and st[-1] == pairs[')']):
+                    st.pop()
+                else:
+                    return False
+            if(s[i] == '}'):
+                if(len(st)!=0 and st[-1] == pairs['}']):
+                    st.pop()
+                else:
+                    return False
+            if(s[i] == ']'):
+                if(len(st)!=0 and st[-1] == pairs[']']):
+                    st.pop()
+                else:
+                    return False
+        if(len(st)>0):
+            return False
+
+
+
+        
+        return True
+        
